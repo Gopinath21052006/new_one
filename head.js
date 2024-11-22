d70648cb1a8d1bdc24be69372e9dd0d059560e13
Gopinath21052006 <gopin6038@gmail.com>
2024-11-22 22:50:07 +0530

    stage 1 for head.html ,head.js

--- head.js
+++ head.js
@@ -1,6 +1,18 @@
 const playButton = document.getElementById('playButton');
 const audio = document.getElementById('myAudio');
-
+const stopButton =document .getElementById('stopButton');
+const rewindButton=document.getElementById('rewindButton');
+const _2xButton =document.getElementById("2xButton")
 playButton.addEventListener('click', () => {
-    audio.play(); 1 
+    audio.play(); 
 });
+stopButton.addEventListener('click',() =>{
+    audio.pause(); 
+});
+rewindButton.addEventListener('click',()=>{
+    audio.currentTime=0
+});
+_2xButton.addEventListener('click', ()=>{
+    audio.playbackRate = 2;
+})
+
