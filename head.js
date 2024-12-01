c60d1a5f28d931843d29ee4faaed91e67dff96cd
Gopinath21052006 <gopin6038@gmail.com>
2024-12-01 20:57:48 +0530

    add all

--- head.js
+++ head.js
@@ -3,14 +3,27 @@ const audio = document.getElementById('myAudio');
 const stopButton =document .getElementById('stopButton');
 const rewindButton=document.getElementById('rewindButton');
 const contolbox=document.getElementById('contolbox');
+const home=document.getElementById("headButton");
+
+home.addEventListener('click',()=>{
+      close();
+}
+)
+
+
 playButton.addEventListener('click', () => {
     audio.play(); 
+    playButton.style = "visibility:hidden;"
+    stopButton.style = "visibility:inhert;"
 });
 stopButton.addEventListener('click',() =>{
     audio.pause(); 
+    stopButton.style = "visibility:hidden;"
+    playButton.style = "visibility:inhert;"
 });
 rewindButton.addEventListener('click',()=>{
     audio.currentTime=0
+    
 });
 
 audio.onloadedmetadata = function(){
